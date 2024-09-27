c4df98916058100e522e3519af12cec3b59a12c9
Matheus Lemos <matheuslemos573@gmail.com>
2024-09-26 22:03:10 -0300

    Exercícios JavaScript (Parte 2)

---
+++ Exercícios JavaScript (Parte 2)/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora =21
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'img/foto-manha.jpg'
+        document.body.style.backgroundImage = 'linear-gradient(rgb( 198, 200, 197), rgb( 248, 240, 229 ), rgb( 119, 136, 91 ), rgb(69, 80, 48) )'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = 'img/foto-tarde.jpg'
+        document.body.style.backgroundImage = 'linear-gradient(rgb(120, 48, 23), rgb(156, 102, 55), rgb(201, 121, 48), rgb(70, 58, 60))'
+    } else {
+        //BOA NOITE
+        img.src = 'img/foto-noite.jpg'
+          document.body.style.backgroundImage = 'linear-gradient(rgb(13, 16, 21), rgb(17, 30, 36), rgb(59, 79, 86), rgb(67, 74, 82))'
+    }
+}
